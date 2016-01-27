4cf94fb200783394673e4c0b09d65be9d442979a
dll <kaixin890312@163.com>
2016-01-27 15:09:01 +0800

    .

--- lib/sorcery/providers/wechat.rb
+++ lib/sorcery/providers/wechat.rb
@@ -42,32 +42,24 @@ module Sorcery
       # calculates and returns the url to which the user should be redirected,
       # to get authenticated at the external provider's site.
       def login_url(params, session)
-        puts params
-        authorize_url
+        authorize_url({ authorize_url: auth_url , appid: key})
       end
 
       # overrides oauth2#authorize_url to allow customized scope.
-      def authorize_url
-
-        # Fix: replace default oauth2 options, specially to prevent the Faraday gem which
-        # concatenates with "/", removing the Facebook api version
-        # options = {
-        #     site:          File::join(@site, api_version.to_s),
-        #     authorize_url: File::join(@auth_site, api_version.to_s, auth_path),
-        #     token_url:     token_url
-        # }
-        #
-        # @scope = access_permissions.present? ? access_permissions.join(',') : scope
-        # super(options)
-        client = build_client(options)
-        client.auth_code.authorize_url(
-            redirect_uri: @callback_url,
-            scope: @scope,
-            display: @display,
-            state: @state,
-            appid: @key
-        )
-      end
+      # def authorize_url
+      #
+      #   # Fix: replace default oauth2 options, specially to prevent the Faraday gem which
+      #   # concatenates with "/", removing the Facebook api version
+      #   options = {
+      #       site:          File::join(@site, api_version.to_s),
+      #       authorize_url: File::join(@auth_site, api_version.to_s, auth_path),
+      #       token_url:     token_url
+      #   }
+      #
+      #   @scope = access_permissions.present? ? access_permissions.join(',') : scope
+      #   super(options)
+      #
+      # end
 
       # tries to login the user from access token
       def process_callback(params, session)

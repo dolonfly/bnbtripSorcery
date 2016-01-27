55e6a6fcfb9447646efd3fa99e0a3d22507ff361
dll <kaixin890312@163.com>
2016-01-27 15:32:00 +0800

    .

--- lib/sorcery/providers/wechat.rb
+++ lib/sorcery/providers/wechat.rb
@@ -12,7 +12,7 @@ module Sorcery
 
       attr_reader   :mode, :param_name, :parse
       attr_accessor :access_permissions, :display, :scope, :token_url,
-                    :user_info_path, :auth_path, :api_version
+                    :user_info_path, :auth_path, :api_version, :key
 
       def initialize
         super
@@ -42,24 +42,25 @@ module Sorcery
       # calculates and returns the url to which the user should be redirected,
       # to get authenticated at the external provider's site.
       def login_url(params, session)
-        authorize_url({ authorize_url: auth_url , appid: key})
+        authorize_url
       end
 
       # overrides oauth2#authorize_url to allow customized scope.
-      # def authorize_url
-      #
-      #   # Fix: replace default oauth2 options, specially to prevent the Faraday gem which
-      #   # concatenates with "/", removing the Facebook api version
-      #   options = {
-      #       site:          File::join(@site, api_version.to_s),
-      #       authorize_url: File::join(@auth_site, api_version.to_s, auth_path),
-      #       token_url:     token_url
-      #   }
-      #
-      #   @scope = access_permissions.present? ? access_permissions.join(',') : scope
-      #   super(options)
-      #
-      # end
+      def authorize_url
+
+        # Fix: replace default oauth2 options, specially to prevent the Faraday gem which
+        # concatenates with "/", removing the Facebook api version
+        options = {
+            site:          File::join(@site, api_version.to_s),
+            authorize_url: File::join(@auth_site, api_version.to_s, auth_path),
+            token_url:     token_url
+        }
+
+        @scope = access_permissions.present? ? access_permissions.join(',') : scope
+        @appid = key
+        super(options)
+
+      end
 
       # tries to login the user from access token
       def process_callback(params, session)
